8ba0c5bd63c78a166ca501f6f5ab600f76099486
= <ulim130@naver.com>
2021-01-09 16:41:33 +0900

    DailyTemperature solve

--- src/lecture_StringAndArray/DailyTemperature_0109.java
+++ src/lecture_StringAndArray/DailyTemperature_0109.java
@@ -1,5 +1,8 @@
 package lecture_StringAndArray;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class DailyTemperature_0109 {
 	
 	/*자신보다 더 큰 온도가 나올 떄 까지 얼마나 걸렸는가를 반환
@@ -11,7 +14,23 @@ public class DailyTemperature_0109 {
 	 */
 	
 	public int[] solve(int[] nums){
-		return nums;
+		
+		// 이용할 스택과 결과물을 담을 배열 선언
+		Stack<Integer> stack = new Stack<>();
+		int[] result = new int[nums.length];
+		
+		for (int i =0;i<nums.length;i++){
+			
+			// 스택이 비어있지 않고 스택에 들어있는 값들이 현재 비교하는 값보다 작은 경우 -> 온도가 올라간 것!
+			while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
+				int index = stack.pop(); 
+				result[index] = i-index;
+			}
+			
+			stack.push(i);
+		}
+		
+		return result; 
 	}
 	
 	
@@ -19,7 +38,7 @@ public class DailyTemperature_0109 {
 		int[] nums = {73, 74, 75, 71, 69, 72, 76, 73};
 		DailyTemperature_0109 a = new DailyTemperature_0109();
 		
-		System.out.println(a.solve(nums));
+		System.out.println(Arrays.toString(a.solve(nums)));
 		
 	}
 }
