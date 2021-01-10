9d911cbbd3b2bf9b7eaefdc390b78231bbd2378b
= <ulim130@naver.com>
2021-01-10 20:38:16 +0900

    MaximumSubarray solve

--- src/lecture_StringAndArray/MaximumSubarray_0110.java
+++ src/lecture_StringAndArray/MaximumSubarray_0110.java
@@ -2,10 +2,20 @@ package lecture_StringAndArray;
 
 public class MaximumSubarray_0110 {
 	
-	/*Subarray중에 제일 큰 수 구하기*/
+	/*Subarray중에 제일 큰 수 구하기
+	  
+	 max값 하나 정해놓고 하나씩 더해나가면서 max보다 크다면 max 교체*/
 	
 	public int solve(int[] nums){
-		return 0;
+		
+		int max=0, newSum=0;
+		for (int num: nums){
+			// 지금까지 더해 온 값에 현재 값을 더하는것 보다 그냥 현재의 값이 더 크다면 현재값을 선택하도록
+			newSum = Math.max(num, newSum+num);
+			max = Math.max(max,newSum);
+		}
+		
+		return max;
 	}
 
 	public static void main(String[] args) {
