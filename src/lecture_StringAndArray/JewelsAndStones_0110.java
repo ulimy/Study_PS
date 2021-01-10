50243afff4e73fb2dbd1df80b4ff32061495c28e
= <ulim130@naver.com>
2021-01-10 14:32:08 +0900

    JewelsAndStones problem

---
+++ src/lecture_StringAndArray/JewelsAndStones_0110.java
@@ -0,0 +1,22 @@
+package lecture_StringAndArray;
+
+public class JewelsAndStones_0110 {
+
+	/* 
+	 J는 Jewel S는 Stone S에 J가 몇개나 들어있는지 대소문자 구분하여 찾기
+	 
+	 input : J="aA" S="aAAbbbb"
+	 output : 3
+	 
+	 */
+	
+	public int solve(String jew,String stone){
+		return 0;
+	}
+	
+	public static void main(String[] args){
+		JewelsAndStones_0110 a = new JewelsAndStones_0110();
+		String J="aA0", S="aAAbbbb";
+		System.out.println(a.solve(J,S));
+	}
+}
