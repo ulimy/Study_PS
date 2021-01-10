adbb87aef8c730c5c3b251de7aab51e39d2479b7
= <ulim130@naver.com>
2021-01-10 16:54:19 +0900

    KClosest problem

---
+++ src/lecture_StringAndArray/KClosest_0110.java
@@ -0,0 +1,23 @@
+package lecture_StringAndArray;
+
+public class KClosest_0110 {
+	
+	/*
+	 원점으로부터 거리가 가장 짧은 점 찾아내기
+	 
+	 input : points = [[1,3],[-2,2]],k=1
+	 output : [[-2,2]]
+	 
+	 */
+
+	public int[][] solve(int[][] points,int k){
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		KClosest_0110 a = new KClosest_0110();
+		System.out.println(a.solve().toString());
+
+	}
+
+}
