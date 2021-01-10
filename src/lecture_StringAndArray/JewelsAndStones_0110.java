a281f360cd973fc1f9fb80304f767e9bfc7f3500
= <ulim130@naver.com>
2021-01-10 14:37:48 +0900

    JewelsAndStones solve

--- src/lecture_StringAndArray/JewelsAndStones_0110.java
+++ src/lecture_StringAndArray/JewelsAndStones_0110.java
@@ -1,5 +1,8 @@
 package lecture_StringAndArray;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class JewelsAndStones_0110 {
 
 	/* 
@@ -8,10 +11,30 @@ public class JewelsAndStones_0110 {
 	 input : J="aA" S="aAAbbbb"
 	 output : 3
 	 
+	 Hashset 이용하기 -> 중복 허용 X이므로 J에 똑같은 문자가 들어있더라도 중복이 없으므로 불필요한 비교를 하지 않게됨
+	 
 	 */
 	
 	public int solve(String jew,String stone){
-		return 0;
+		
+		Set<Character> set = new HashSet<>();
+		
+		// 1) jew를 char하나하나로 쪼개 set에 담기
+		for(char jewChar:jew.toCharArray()){
+			set.add(jewChar);
+		}
+		
+		// 2) 개수를 담을 변수
+		int count=0;
+		
+		// 3) stone을 char하나하나로 쪼개서 jew를 담아둔 set에 존재하는지 확인
+		for(char stoneChar:stone.toCharArray()){
+			if(set.contains(stoneChar)){
+				count++;
+			}
+		}
+		
+		return count;
 	}
 	
 	public static void main(String[] args){
