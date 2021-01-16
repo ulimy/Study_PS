683e51a20f995a7e3959e3859ea6d6955e478761
= <ulim130@naver.com>
2021-01-16 17:12:43 +0900

    FindAnagramsMapping solve

--- src/lecture_StringAndArray/FindAnagramsMapping_0116.java
+++ src/lecture_StringAndArray/FindAnagramsMapping_0116.java
@@ -1,6 +1,8 @@
 package lecture_StringAndArray;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindAnagramsMapping_0116 {
 	
@@ -10,11 +12,28 @@ public class FindAnagramsMapping_0116 {
 	
 	 input : int[] A = {11,27,45,31,50} int[] B = {50,11,31,45,27}
 	 output : [1,4,3,2,0]
+	 
+	 Map 이용하기!
+	 Map에 B값과 그 위치를 저장하고 A의 순서대로 꺼내기
 
 	 */
 	
 	public int[] solve(int[] A, int[] B){
-		return null;
+		// 결과값을 담을 배열
+		int[] result = new int[A.length];
+		Map<Integer,Integer> map = new HashMap<>();
+		
+		//B 저장
+		for (int i=0;i<A.length;i++){
+			map.put(B[i],i);
+		}
+		
+		//A로 꺼내기
+		for (int i=0;i<A.length;i++){
+			result[i]=map.get(A[i]);
+		}
+		
+		return result;
 	}
 
 	public static void main(String[] args) {
