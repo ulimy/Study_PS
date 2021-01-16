3ed1e9d68147678d7b43eae321f749ebbed3890f
= <ulim130@naver.com>
2021-01-16 20:35:47 +0900

    GroupAnagrams solve

--- src/lecture_StringAndArray/GroupAnagrams_0116.java
+++ src/lecture_StringAndArray/GroupAnagrams_0116.java
@@ -1,6 +1,10 @@
 package lecture_StringAndArray;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GroupAnagrams_0116 {
 	
@@ -13,7 +17,36 @@ public class GroupAnagrams_0116 {
 	 */
 	
 	public List<List<String>> solve(String[] strs){
-		return null;
+		List<List<String>> result = new ArrayList<>();
+		
+		// 비어있는지 확인
+		if(strs==null || strs.length==0) return result;
+		
+		Map<String,List<String>> map = new HashMap<>();
+		
+		for(String str:strs){
+			// char배열로 바꿔 정렬하면 순서가 다른것들도 다 같아짐
+			char[] charArray = str.toCharArray();
+			Arrays.sort(charArray);
+			
+			// 정렬한 문자열을 key로 사용하여 map에 대응시키기
+			String key = String.valueOf(charArray);
+			
+			// 이미 map에 있다면? list에 추가
+			if(map.containsKey(key)){
+				map.get(key).add(str);
+			}else{
+				// 없다면 한번도 나오지 않는것. 새로 생성.
+				List<String> list = new ArrayList<>();
+				list.add(str);
+				map.put(key, list);
+			}
+		}
+		
+		// map의 value들만 result에 추가하여 리턴
+		result.addAll(map.values());
+		
+		return result;
 	}
 	
 	
