74343dc50c8173297f1702218e8a75288849108a
= <ulim130@naver.com>
2021-01-16 20:08:00 +0900

    SpiralMatrix problem:

--- README.md
+++ README.md
@@ -20,3 +20,4 @@
  - [MaximumSubarray](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/MaximumSubarray_0110.java)
  - [FindAnagramsMapping](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/FindAnagramsMapping_0116.java)
  - [FindAllAnagrams](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/FindAllAnagrams_0116.java)
+ - [SpiralMatrix](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/SpiralMatrix_0116.java)
---
+++ src/lecture_StringAndArray/SpiralMatrix_0116.java
@@ -0,0 +1,27 @@
+package lecture_StringAndArray;
+
+import java.util.List;
+
+public class SpiralMatrix_0116 {
+	/*
+	 나선형 모양으로 배열 출력하기
+	 
+	 input : int[][] matrix = { {1,2,3},{4,5,6},{7,8,9} };
+	 output : {1,2,3,6,9,8,7,4,5}
+	 
+	 */
+	
+	public List<Integer> solve(int[][] matrix){
+		return null;
+	}
+
+	public static void main(String[] args) {
+		SpiralMatrix_0116 a = new SpiralMatrix_0116();
+		int[][] matrix = { {1,2,3},
+	   					   {4,5,6},
+						   {7,8,9} };
+		System.out.println(a.solve(matrix).toString());
+
+	}
+
+}
