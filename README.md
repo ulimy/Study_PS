21dcedf390dd4226ea72df2e370fa1874b7405fb
= <ulim130@naver.com>
2021-01-23 13:45:38 +0900

    TrappingRainWater problem

--- README.md
+++ README.md
@@ -22,3 +22,4 @@
  - [FindAllAnagrams](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/FindAllAnagrams_0116.java)
  - [SpiralMatrix](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/SpiralMatrix_0116.java)
  - [GroupAnagrams](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/GroupAnagrams_0116.java)
+ - [TrappingRainWater](https://github.com/ulimy/Study_PS/blob/master/src/lecture_StringAndArray/TrappingRainWater_0123.java)
---
+++ src/lecture_StringAndArray/TrappingRainWater_0123.java
@@ -0,0 +1,27 @@
+package lecture_StringAndArray;
+
+public class TrappingRainWater_0123 {
+	
+	/* 벽돌에 채워지는 빗물
+	
+	1. 왼쪽 벽 높이 모두 구하기
+	2. 오른쪽 벽 모두 구하기
+	3. 1과 2중 작은 값을 구하면 그게 높이
+	4. 벽돌 높이 구하기 (벽돌이 바닥에 있는 경우 빼줘야 함)
+	5. 3에서 5빼면 그게 최종 높이
+	
+	
+	*/
+	
+	public int solve(int[] height){
+		
+		return 0;
+	}
+	
+	public static void main(String args[]){
+		TrappingRainWater_0123 a = new TrappingRainWater_0123();
+		int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+		System.out.println(a.solve(nums));
+	}
+
+}
