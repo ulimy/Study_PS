61b2c4dccc4dbe98ac7ef51c949577d4c8bac08d
= <ulim130@naver.com>
2021-01-23 19:10:47 +0900

    Valid Parentheses solve

--- src/lecture_QueueAndStack/ValidParentheses_0123.java
+++ src/lecture_QueueAndStack/ValidParentheses_0123.java
@@ -1,10 +1,35 @@
 package lecture_QueueAndStack;
 
+import java.util.Stack;
+
 public class ValidParentheses_0123 {
 	// 괄호 확인
 	
 	public boolean solve(String str){
-		return false;
+		
+		// 길이가 홀수라면 당연히 false
+		if(str.length()%2!=0) return false;
+		
+		Stack<Character> stack = new Stack<>();
+		
+		for(int i=0;i<str.length();i++){
+			switch(str.charAt(i)){
+			case ')':
+				if(!stack.empty() && stack.peek()=='(') stack.pop();
+				break;
+			case ']':
+				if(!stack.empty() && stack.peek()=='[') stack.pop();
+				break;
+			case '}':
+				if(!stack.empty() && stack.peek()=='{') stack.pop();
+				break;
+		    default :
+		    	stack.push(str.charAt(i));
+			}
+		}
+		
+		// 비어있으면 다 맞음으로 true
+		return stack.empty();
 	}
 	
 	public static void main(String[] args){
