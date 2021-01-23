9a17dbc8ca8aad82199f03b8942036ef455338d3
= <ulim130@naver.com>
2021-01-23 16:28:44 +0900

    ReverseLinkedList solve

--- src/lecture_LinkedList/ReverseLinkedList_0123.java
+++ src/lecture_LinkedList/ReverseLinkedList_0123.java
@@ -4,8 +4,22 @@ public class ReverseLinkedList_0123 {
 	
 	// Linked list 뒤집기
 	
-	public ListNode solve(ListNode ln){
-		return null;
+	public ListNode solve(ListNode cur){
+		// 맨 처음 노드를 맨 뒤로 보내고 뒤에서 앞으로 하나씩 추가해 간다고 생각하기
+		ListNode next=null;
+		ListNode prev=null;
+		
+		while(cur!=null){
+			// 다음 노트를 next에 잠시 저장
+			next = cur.next;
+			// cur의 next는 prev로
+			cur.next = prev;
+			// 추가했으니 앞으로 한칸 이동
+			prev=cur;
+			cur = next;
+		}
+		
+		return prev;
 	}
 	
 	public void printNode(ListNode head) {
