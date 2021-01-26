eb9982c9decda937a2bb26a0a1e1032fd0b9e224
= <ulim130@naver.com>
2021-01-26 15:50:10 +0900

    MaximumDepthOfBinaryTree solve

--- src/lecture_Graph/MaximumDepthOfBinaryTree_0126.java
+++ src/lecture_Graph/MaximumDepthOfBinaryTree_0126.java
@@ -13,7 +13,16 @@ class TreeNode{
 public class MaximumDepthOfBinaryTree_0126 {
 	
 	public int solve(TreeNode root){
-	
+		
+		// 널체크
+		if (root==null) return 0;
+		
+		// 재귀호출 이용. 위에서 널체크 하므로 널이면 재귀호출 종료
+		int leftMax = solve(root.left);
+		int rightMax = solve(root.right);
+		
+		// 비교 후 큰 값+1 (맨 처음 -> 양쪽 둘다 null -> 0+1=1)
+		return Math.max(leftMax, rightMax)+1;
 	}
 
 	public static void main(String[] args) {
