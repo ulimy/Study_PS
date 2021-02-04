18a3429fcc1fe8392ba2044a6c202b2fb23dfcc6
= <ulim130@naver.com>
2021-02-04 10:18:30 +0900

    LongestIncreasingSubsequence solve

--- src/lecture_DynamicProgramming/LongestIncreasingSubsequence_0204.java
+++ src/lecture_DynamicProgramming/LongestIncreasingSubsequence_0204.java
@@ -1,11 +1,28 @@
 package lecture_DynamicProgramming;
 
+import java.util.Arrays;
+
 public class LongestIncreasingSubsequence_0204 {
 
 	// 증가가 계속되는 원소의 개수
 	
 	public int solve(int[] nums){
-		return 0;
+		
+		// 널체크
+		if (nums==null || nums.length==0) return 0;
+		
+		int[] dp = new int[nums.length];
+		Arrays.fill(dp, 1);
+		
+		for (int i=1;i<nums.length;i++){
+			if(nums[i]>nums[i-1]){
+				dp[i] = dp[i-1]+1;
+			}else{
+				dp[i] = dp[i-1];
+			}
+		}
+		
+		return dp[nums.length-1];
 	}
 	
 	public static void main(String[] args) {
