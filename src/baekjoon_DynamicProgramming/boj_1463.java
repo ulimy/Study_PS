85517a44a00de15073c471eed57e43652c35044b
ulimy <ulim130@gmail.com>
2021-03-04 14:25:07 +0900

    1463

--- src/baekjoon_DynamicProgramming/boj_1463.java
+++ src/baekjoon_DynamicProgramming/boj_1463.java
@@ -1,8 +1,37 @@
 package baekjoon_DynamicProgramming;
 
-public class boj_1463 {
+import java.util.Scanner;
 
+public class boj_1463 {
+	
+	/*
+	정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
+
+	1. X가 3으로 나누어 떨어지면, 3으로 나눈다.
+	2. X가 2로 나누어 떨어지면, 2로 나눈다.
+	3. 1을 뺀다.
+	
+	정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 
+	연산을 사용하는 횟수의 최솟값을 출력하시오.
+	*/
+	
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		sc.close();
+		
+		int dp[] = new int[x+1];
+		dp[0]=0;
+		dp[1]=0;
+		
+		for (int i=2;i<=x;i++){
+			dp[i]=dp[i-1]+1;
+			if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
+			if(i%2==0) dp[i] = Math.min(dp[i],dp[i/2]+1);
+		}
+		
+		System.out.println(dp[x]);
 		
 	}
 
