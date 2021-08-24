06c921077feab464c89e1f1a60d89faf8c42f877
= <ulim130@naver.com>
2021-08-24 18:21:29 +0900

    refactor

--- src/baekjoon_Study/Step14.java
+++ src/baekjoon_Study/Step14.java
@@ -1,13 +1,12 @@
 package baekjoon_Study;
 
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Step14 {
 	
 	static int M,N;
 	static int[] arr;
+	static StringBuilder result = new StringBuilder();
 	
 	static boolean[] visited;
 	
@@ -15,11 +14,8 @@ public class Step14 {
 		
 		// count가 M이라면 배열이 가득  찼으므로 print
 		if(count==M){
-			StringBuilder result = new StringBuilder();
-			for(int a : arr){
-				result.append(a+" ");
-			}
-			System.out.println(result.toString());
+			for(int a : arr) result.append(a+" ");
+			result.append("\n");
 			return;
 		}
 		
@@ -44,13 +40,12 @@ public class Step14 {
 
 	public static void p_15650(int count,int num){
 		if(count==M){
-			StringBuilder result = new StringBuilder();
 			for(int a : arr) result.append(a+" ");
 			System.out.println(result.toString());
 			return;
 		}
 		
-		// 오름차순이므로 앞에서 정한 숫자보다 큰 숫자들 중에서만 고르기!
+		// 오름차순이므로 앞에서 정한 숫자보다 큰 숫자들 중에서만 고르기
 		for(int i=num+1;i<=N;i++){
 			arr[count] = i;
 			p_15650(count+1,i);
@@ -58,21 +53,12 @@ public class Step14 {
 		
 		return;
 	}
-	
-	static Set<int[]> set = new HashSet<>();
-	static StringBuilder result = new StringBuilder();
+
 	
 	public static void p_15651(int count){
-		// arr이 다 채워졌다면 set으로 중복확인
 		if(count==M){
-			// 중복 아니라면 set에 넣고 출력
-			if(!set.contains(arr)){
-				// 주소값 기반이므로 arr을 넣으면 값이 바뀐 arr이 다르다고 인식 불가 -> 복사해서 넣어주기
-				set.add(arr.clone());
-				for(int a : arr) result.append(a+" ");
-				result.append("\n");
-			}
-			// 중복이라면 그냥 return
+			for(int a : arr) result.append(a+" ");
+			result.append("\n");
 			return;
 		}
 
@@ -88,13 +74,12 @@ public class Step14 {
 	
 	public static void p_15652(int count,int num){
 		if(count==M){
-			StringBuilder result = new StringBuilder();
 			for(int a : arr) result.append(a+" ");
-			System.out.println(result.toString());
+			result.append("\n");
 			return;
 		}
 		
-		// 오름차순이므로 앞에서 정한 숫자보다 큰 숫자들 중에서만 고르기!
+		// 오름차순이므로 앞에서 정한 숫자보다 큰 숫자들 중에서만 고르기
 		// 같은 숫자를 골라도 되므로 num도 포함
 		for(int i=num;i<=N;i++){
 			arr[count] = i;
@@ -114,14 +99,21 @@ public class Step14 {
 		
 		// 1부터 N까지의 방문여부
 		//visited = new boolean[N+1];
+		//result.setLength(0);
 		//p_15649(0);
+		//System.out.println(result.toString());
 		
+		//result.setLength(0);
 		//p_15650(0,0);
+		//System.out.println(result.toString());
 		
+		//result.setLength(0);
 		//p_15651(0);
 		//System.out.println(result.toString());
 		
-		p_15652(0,1);
+		//result.setLength(0);
+		//p_15652(0,1);
+		//System.out.println(result.toString());
 		
 		sc.close();
 		
