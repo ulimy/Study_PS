86d6b1cfe97d4118cf5a11b6b473c204226f6758
= <ulim130@naver.com>
2021-09-10 19:14:03 +0900

    9184

--- src/baekjoon_Study/Step15.java
+++ src/baekjoon_Study/Step15.java
@@ -7,7 +7,7 @@ public class Step15 {
 
 	// 피보나치
 	public static void p_1003() {
-		
+
 		int[] fibonacci_zero = new int[41];
 		int[] fibonacci_one = new int[41];
 
@@ -33,10 +33,70 @@ public class Step15 {
 		sc.close();
 
 		System.out.println(result.toString());
+		return;
+	}
+
+	// 신나는 함수 실행
+	static int[][][] exciting = new int[21][21][21];
+
+	public static boolean checkRange(int a, int b, int c) {
+		return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
+	}
+
+	public static int w(int a, int b, int c) {
+
+		// a,b,c가 배열 크기보다 작고 이미 계산된 적 있다면 바로 리턴
+		if (checkRange(a, b, c) && exciting[a][b][c] != 0)
+			return exciting[a][b][c];
+
+		// 조건 1
+		if (a <= 0 || b <= 0 || c <= 0)
+			return 1;
+		
+		// 조건 2
+		if (a > 20 || b > 20 || c > 20) { 
+			exciting[20][20][20] = w(20, 20, 20);
+			return exciting[20][20][20];
+		} 
+		
+		if (a < b && b < c) { // 조건 3
+			exciting[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+		} else { // 조건 4
+			exciting[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+		}
+
+		return exciting[a][b][c];
+
+	}
+
+	public static void p_9184() {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder result = new StringBuilder();
+
+		while (true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+
+			if (a == -1 && b == -1 && c == -1)
+				break;
+
+			result.append("w("+a+", "+b+", "+c+") = ");
+			result.append(w(a, b, c) + "\n");
+
+		}
+
+		sc.close();
+
+		System.out.println(result.toString());
+		return;
 	}
 
 	public static void main(String[] args) {
-		p_1003();
+		// p_1003();
+		p_9184();
+		
+		return;
 	}
 
 }
