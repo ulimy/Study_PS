7d66d021ec5ec390f230d1da3d0b1357d9c6e2ec
= <ulim130@naver.com>
2021-09-23 20:53:02 +0900

    1931

--- src/baekjoon_Study/Step16.java
+++ src/baekjoon_Study/Step16.java
@@ -1,5 +1,7 @@
 package baekjoon_Study;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Step16 {
@@ -33,8 +35,42 @@ public class Step16 {
 
 	}
 
+	// 회의실 배정
+	public static void p_1931() {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		int[][] meetings = new int[count][2];
+
+		for (int i = 0; i < count; i++) {
+			meetings[i][0] = sc.nextInt();
+			meetings[i][1] = sc.nextInt();
+		}
+
+		sc.close();
+
+		Arrays.sort(meetings, new Comparator<int[]>(){
+			@Override
+			public int compare(int[] a, int[] b){
+				return (a[1]==b[1])?a[0]-b[0]:a[1]-b[1];
+			}
+		});
+
+		int pre_end = 0;
+		int result = 0;
+		
+		for (int i = 0; i < count; i++) {
+			if (pre_end <= meetings[i][0]) {
+				pre_end = meetings[i][1];
+				result++;
+			}
+		}
+
+		System.out.println(result);
+	}
+
 	public static void main(String[] args) {
-		p_11047();
+		// p_11047();
+		p_1931();
 		return;
 	}
 
