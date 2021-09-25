8b671b530aefe24b5462bd51e45c904ebd79e460
= <ulim130@naver.com>
2021-09-25 16:10:45 +0900

    2581

--- src/baekjoon_Study/Step9.java
+++ src/baekjoon_Study/Step9.java
@@ -32,7 +32,8 @@ public class Step9 {
 				}
 			}
 
-			if(check) result++;
+			if (check)
+				result++;
 		}
 
 		System.out.println(result);
@@ -41,8 +42,56 @@ public class Step9 {
 
 	}
 
+	// 소수
+	public static void p_2581() {
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt();
+		int n = sc.nextInt();
+		sc.close();
+
+		// 소수라면 false / 아니라면 true
+		boolean[] arr = new boolean[n + 1]; // 초기값 false
+		arr[0] = true;
+		arr[1] = true;
+
+		// 2~n까지 에라토스테네스의 체 이용
+		for (int i = 2; i * i <= n; i++) {
+			// 소수라면 소수의 배수들 합성수 처리 해주기
+			if (!arr[i]) {
+				int index = i * 2;
+				while (index <= n) {
+					arr[index] = true;
+					index += i;
+				}
+			}
+		}
+
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+
+		for (int i = m; i <= n; i++) {
+			if (!arr[i]) {
+				sum += i;
+				if (min == Integer.MAX_VALUE)
+					min = i;
+			}
+		}
+
+		if (sum > 0) {
+			System.out.println(sum + "\n" + min);
+		} else {
+			System.out.println("-1");
+		}
+
+		return;
+
+	}
+	
+	
+
 	public static void main(String[] args) {
-		p_1978();
+		// p_1978();
+		p_2581();
 		return;
 	}
 
