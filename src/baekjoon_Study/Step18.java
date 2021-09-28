5bac1a7142da2b10dfb8f7b9e3f42a723ef44076
= <ulim130@naver.com>
2021-09-28 18:18:39 +0900

    4949

--- src/baekjoon_Study/Step18.java
+++ src/baekjoon_Study/Step18.java
@@ -74,9 +74,96 @@ public class Step18 {
 		return;
 	}
 
+	// 괄호
+	public static void p_9012() {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+
+			char[] input = sc.next().toCharArray();
+			Stack<Character> stack = new Stack<>();
+			boolean check = true;
+
+			for (char c : input) {
+				if (c == '(') {
+					stack.push(c);
+				} else {
+					// 괄호를 닫아야 하는데 여는 기호가 없다면 올바르지 않음
+					if (stack.isEmpty()) {
+						check = false;
+						break;
+					}
+					stack.pop();
+				}
+			}
+
+			result.append(((stack.isEmpty() && check) ? "YES" : "NO") + "\n");
+
+		}
+
+		sc.close();
+
+		System.out.println(result.toString());
+
+		return;
+	}
+
+	// 균형잡힌 세상
+	public static void p_4949() {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder result = new StringBuilder();
+		String input = sc.nextLine();
+
+		while (!input.equals(".")) {
+
+			input = input.replace(".", ""); // 마침표 지우기
+			input = input.replaceAll("[a-zA-Z]", ""); // 알파벳 지우기
+			input = input.replaceAll(" ", "");// 공백 지우기
+
+			Stack<Character> stack = new Stack<>();
+			boolean check = true;
+
+			for (char c : input.toString().toCharArray()) {
+
+				if (c == '(' || c == '[') {
+					stack.push(c);
+				} else if (c == ')') {
+					if (stack.isEmpty() || stack.peek() != '(') {
+						check = false;
+						break;
+					} else {
+						stack.pop();
+					}
+				} else {
+					if (stack.isEmpty() || stack.peek() != '[') {
+						check = false;
+						break;
+					} else {
+						stack.pop();
+					}
+				}
+			}
+
+			result.append(((stack.isEmpty() && check) ? "yes" : "no") + "\n");
+
+			input = sc.nextLine();
+		}
+
+		sc.close();
+
+		System.out.println(result.toString());
+
+		return;
+
+	}
+
 	public static void main(String[] args) {
 		// p_10828();
-		p_10773();
+		// p_10773();
+		// p_9012();
+		p_4949();
 
 		return;
 	}
