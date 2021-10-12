908411adf1bd5f01f9a9eb770fa6c8eb23d8f704
= <ulim130@naver.com>
2021-10-12 15:33:39 +0900

    1992

--- src/baekjoon_Study/Step20.java
+++ src/baekjoon_Study/Step20.java
@@ -57,9 +57,58 @@ public class Step20 {
 		return;
 	}
 
-	public static void main(String[] args) {
-		p_2630();
+	// 쿼드 트리
+	public static int[][] video;
+	public static StringBuilder quad_tree;
 
+	public static void quad(int row, int col, int size) {
+		int compare = video[row][col];
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (video[i + row][j + col] != compare) {
+					size /= 2;
+					quad_tree.append("(");
+					quad(row, col, size); // 왼쪽 위
+					quad(row, col + size, size); // 오른쪽 위
+					quad(row + size, col, size);// 왼쪽 아래
+					quad(row + size, col + size, size);// 오른쪽 아래
+					quad_tree.append(")");
+					return;
+				}
+			}
+		}
+
+		quad_tree.append(compare);
+
+		return;
+	}
+
+	public static void p_1992() {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		video = new int[n][n];
+		quad_tree = new StringBuilder();
+
+		for (int i = 0; i < n; i++) {
+			String input = sc.nextLine();
+			for (int j = n - 1; j >= 0; j--) {
+				video[i][j] = input.charAt(j) - '0';
+			}
+		}
+
+		sc.close();
+
+		quad(0, 0, n);
+
+		System.out.println(quad_tree.toString());
+
+		return;
+	}
+
+	public static void main(String[] args) {
+		// p_2630();
+		p_1992();
 		return;
 	}
 
