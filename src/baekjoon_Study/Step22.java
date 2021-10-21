28e189db96596143b872d84270584691dab0d1e6
= <ulim130@naver.com>
2021-10-21 15:40:03 +0900

    1655

--- src/baekjoon_Study/Step22.java
+++ src/baekjoon_Study/Step22.java
@@ -109,10 +109,56 @@ public class Step22 {
 		return;
 	}
 
+	// 가운데를 말해요
+	public static void p_1655() {
+		Scanner sc = new Scanner(System.in);
+		int count = sc.nextInt();
+
+		// 왼쪽 max
+		PriorityQueue<Integer> left = new PriorityQueue<>((a, b) -> b - a);
+		// 오른쪽 min
+		PriorityQueue<Integer> right = new PriorityQueue<>();
+
+		StringBuilder result = new StringBuilder();
+
+		for (int i = 0; i < count; i++) {
+			int target = sc.nextInt();
+
+			// 크기가 같으면 왼쪽에 넣기
+			if (right.size() == left.size()) {
+				left.offer(target);
+			}
+			// 크기가 다르면 오른쪽에 넣기
+			else {
+				right.offer(target);
+			}
+
+			if (!right.isEmpty() && !left.isEmpty()) {
+				// right의 최소값이 left의 최대값보다 작다면 swap
+				if (right.peek() < left.peek()) {
+					int temp = right.poll();
+					right.offer(left.poll());
+					left.offer(temp);
+				}
+			}
+
+			result.append(left.peek() + "\n");
+
+		}
+
+		sc.close();
+
+		System.out.println(result.toString());
+
+		return;
+
+	}
+
 	public static void main(String[] args) {
 		// p_11279();
 		// p_1927();
-		p_11286();
+		// p_11286();
+		p_1655();
 
 		return;
 	}
