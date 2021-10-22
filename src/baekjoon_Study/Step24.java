28b0d2a54396308ae58638b468a76ba49cfa1c73
= <ulim130@naver.com>
2021-10-22 20:08:14 +0900

    2606

--- src/baekjoon_Study/Step24.java
+++ src/baekjoon_Study/Step24.java
@@ -88,8 +88,59 @@ public class Step24 {
 		return;
 	}
 
+	// 바이러스
+	public static void p_2606() {
+		Scanner sc = new Scanner(System.in);
+		int computer = sc.nextInt();
+		int count = sc.nextInt();
+		int[][] lines = new int[computer + 1][computer + 1];
+
+		for (int i = 0; i < count; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+
+			lines[x][y] = lines[y][x] = 1;
+		}
+
+		sc.close();
+
+		System.out.println(bfs_2606(computer, lines));
+
+	}
+
+	public static int bfs_2606(int n, int[][] lines) {
+
+		int result = 0;
+
+		boolean[] visited = new boolean[n + 1];
+		Queue<Integer> queue = new LinkedList<>();
+
+		// 초기화
+		Arrays.fill(visited, false);
+		queue.offer(1);
+		visited[1] = true;
+
+		// bfs 시작
+		while (!queue.isEmpty()) {
+
+			int target = queue.poll();
+
+			for (int i = 1; i <= n; i++) {
+				// 연결되어있고 방문하지 않았다면
+				if (lines[target][i] == 1 && !visited[i]) {
+					queue.offer(i);
+					visited[i] = true;
+					result++;
+				}
+			}
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) {
-		p_1260();
+		// p_1260();
+		p_2606();
 
 		return;
 	}
